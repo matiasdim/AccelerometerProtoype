2013cc1d9b491503f41edfc3973f9c6e8d616424
Matías <matiasdim3@gmail.com>
2017-09-21 09:32:30 -0400

    Thinking ways to compare

--- AccelerometerProtoype/ViewController.swift
+++ AccelerometerProtoype/ViewController.swift
@@ -14,11 +14,20 @@ class ViewController: UIViewController {
     var motionManager: CMMotionManager!
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var sliderLabel: UILabel!
+    var xData:[Double] = []
+    var yData:[Double] = []
+    var zData:[Double] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.motionManager = CMMotionManager()
-        self.slider.value = 60
+        self.slider.value = Float(60)
+        self.sliderLabel.text = "60"
+        self.slider.maximumValue = 100
+        self.slider.minimumValue = 0
+        self.slider.addTarget(self, action: #selector(sliderValueChanged), for: .valueChanged)
+
     }
     
     override func didReceiveMemoryWarning() {
@@ -26,14 +35,13 @@ class ViewController: UIViewController {
         
     }
     
-    
     @IBAction func clearTextView(_ sender: Any) {
         self.textView.text = ""
     }
     
     @IBAction func startPressed(_ sender: Any) {
-        let updateInterval = 0.01 + 0.005 * self.slider.value;
-        self.motionManager.accelerometerUpdateInterval = updateInterval //1.0 / 60.0  // 60 Hz
+        //let updateInterval = 0.01 + 0.005 * self.slider.value;
+        self.motionManager.accelerometerUpdateInterval = TimeInterval(1/self.slider.value) //1.0 / 60.0  // 60 Hz
         if self.motionManager.isAccelerometerAvailable {
             self.motionManager.startAccelerometerUpdates(to: .main, withHandler: { (accelerometerData, error) in
                 guard let data = accelerometerData else{
@@ -41,8 +49,11 @@ class ViewController: UIViewController {
                     return
                 }
                 let x = data.acceleration.x
+                self.xData.append(x)
                 let y = data.acceleration.y
+                self.yData.append(y)
                 let z = data.acceleration.z
+                self.zData.append(z)
                 self.textView.text = self.textView.text + "X: \(x) \nY: \(y) \nZ: \(z) \n\n"
                 //self.textView.setContentOffset(CGPoint(x: 0, y: 0), animated: false)
                 let range = NSMakeRange(self.textView.text.characters.count - 1, 0)
@@ -59,6 +70,16 @@ class ViewController: UIViewController {
         if self.motionManager.isAccelerometerActive{
             self.motionManager.stopAccelerometerUpdates()
         }
+    }
+    
+    @IBAction func sliderValueChanged(_ slider: UISlider) {
+        self.slider.value = self.slider.value
+        self.sliderLabel.text = "\(self.slider.value)"
+    }
+    
+    func compare(x:Double, y:Double, z:Double) -> Bool
+    {
+        return true
     }
 }
 
