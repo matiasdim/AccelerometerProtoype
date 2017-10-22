bbe25644807579f4eea8fcaa0107ce5c9e6bd4ca
Matías <matiasdim3@gmail.com>
2017-10-22 13:48:05 -0400

    Added sound files

---
+++ AccelerometerProtoype/SoundManager.swift
@@ -0,0 +1,13 @@
+//
+//  SoundManager.swift
+//  AccelerometerProtoype
+//
+//  Created by Matias on 10/22/17.
+//  Copyright © 2017 Matias. All rights reserved.
+//
+
+import UIKit
+
+class SoundManager: NSObject {
+
+}
