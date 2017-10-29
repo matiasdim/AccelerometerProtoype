6f1033c39fa5e7e7bfaf1178cbe1b87dbce0f1e4
Matías <matiasdim3@gmail.com>
2017-10-29 17:32:08 -0400

    Added function to add ranges from view

--- AccelerometerProtoype/ViewController.swift
+++ AccelerometerProtoype/ViewController.swift
@@ -11,18 +11,6 @@ import CoreMotion
 import AVFoundation
 
 struct Constants {
-    // Range of Amplitude T
-    // G force over Y that will tell us how hard the movement is done
-    static let positiveTMax: Double = 9
-    static let positiveTMin: Double = 8.0
-    static let negativeTMin: Double = -9
-    static let negativeTMax: Double = -8.0
-    // Range for frequency
-    // Frequency taken from dataset follows an order of : 1 cycle/ number of measures
-    // number of measures to complete a cycle for the dataset, Approx = 22.5
-    // Frequency = 1/ 22.5 = 0.4
-    static let fMax: Double = 0.06
-    static let fMin: Double = 0.02
     static let accInterval = 1/100 // 100 Hz
     //sound Names
     static let finishedFileName = "finished"
@@ -34,12 +22,23 @@ struct Constants {
     static let youCanDoBetterFileName = "youCanDoBetter"
 }
 
+extension String {
+    var doubleValue: Double {
+        return Double(self) ?? 0
+    }
+}
+
 class ViewController: UIViewController {
     
+    @IBOutlet weak var positiveTMinText: UITextField!
+    @IBOutlet weak var positiveTMaxText: UITextField!
+    @IBOutlet weak var negativeTMinText: UITextField!
+    @IBOutlet weak var negativeTMaxText: UITextField!
+    @IBOutlet weak var fMaxText: UITextField!
+    @IBOutlet weak var fMinText: UITextField!
+    @IBOutlet weak var completionScoreText: UITextField!
+    
     var motionManager: CMMotionManager!
-    @IBOutlet weak var textView: UITextView!
-    @IBOutlet weak var slider: UISlider!
-    @IBOutlet weak var sliderLabel: UILabel!
     
     // For frequency
     // Frequency taken from dataset follows an order of : 1 cycle/ number of measures
@@ -52,7 +51,7 @@ class ViewController: UIViewController {
     // To count 3s before beginning analyzing accelerometer data at the beginning
     var secondsToBegin = 0
     // Initial 3 seconds delay timer
-    var timer = Timer()
+    var initialTimer = Timer()
     
     // Global variables
     var globalScore = 0
@@ -60,7 +59,7 @@ class ViewController: UIViewController {
     var globalFrequency: Double = 0
     // Validator timer
     // Every second checks global values (globalMagnitude, globalFrequency) in order to update global score
-    // It is invalidated when the globalScores is 60
+    // It is invalidated when the globalScores is equal to completionScore(input from view)
     var globalTimer = Timer()
     // To save the points of a particular period
     var periodPoints: [Double] = []
@@ -78,26 +77,36 @@ class ViewController: UIViewController {
     var thatsItAudio: AVAudioPlayer!
     var youCanDoBetterAudio: AVAudioPlayer!
     
+    // Range of Amplitude T
+    // G force over Y that will tell us how hard the movement is done
+    var positiveTMax: Double = 0.0
+    var positiveTMin: Double = 0.0
+    var negativeTMin: Double = 0.0
+    var negativeTMax: Double = 0.0
+    // Range for frequency
+    // Frequency taken from dataset follows an order of : 1 cycle/ number of measures
+    // number of measures to complete a cycle for the dataset, Approx = 22.5
+    // Frequency = 1/ 22.5 = 0.4
+    var fMax: Double = 0.0
+    var fMin: Double = 0.0
+    
+    var completionScore = 60
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.motionManager = CMMotionManager()
-        self.slider.value = Float(60)
-        self.sliderLabel.text = "60"
-        self.slider.maximumValue = 100
-        self.slider.minimumValue = 0
-        self.slider.addTarget(self, action: #selector(sliderValueChanged), for: .valueChanged)
         
         //Audios inits
         initAudios()
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
-    @IBAction func clearTextView(_ sender: Any) {
-        self.textView.text = ""
-    }
+
     func initAudios(){
         finishedAudio = SoundManager.setupSound(soundName: Constants.finishedFileName)
         goodJobAudio = SoundManager.setupSound(soundName: Constants.goodJobFileName)
@@ -146,9 +155,9 @@ class ViewController: UIViewController {
     //Checks every second global values to update global score if needed
     @objc func thickHandler() {
         totalTime += 1
-        if (globalFrequency <= Constants.fMax && globalFrequency >= Constants.fMin &&
-            globalMagnitude.maxVal <= Constants.positiveTMax && globalMagnitude.maxVal >= Constants.positiveTMin &&
-            globalMagnitude.minVal <= Constants.negativeTMax && globalMagnitude.minVal >= Constants.negativeTMin)
+        if (globalFrequency <= fMax && globalFrequency >= fMin &&
+            globalMagnitude.maxVal <= positiveTMax && globalMagnitude.maxVal >= positiveTMin &&
+            globalMagnitude.minVal <= negativeTMax && globalMagnitude.minVal >= negativeTMin)
         {
             wrongMovesCounter = 0
             globalScore += 1
@@ -161,26 +170,37 @@ class ViewController: UIViewController {
                 wrongMovesCounter = 0 // Toonly play "You can do better once"
                 youCanDoBetterAudio.play()
             }else{
-                if (globalFrequency > Constants.fMax || globalFrequency < Constants.fMin){
+                if (globalFrequency > fMax || globalFrequency < fMin){
                     shakeFasterAudio.play()
                 }else{
                     shakeHarderAudio.play()
                 }
             }
         }
-        if(globalScore == 60)
+        if(globalScore == completionScore)
         {
             print("Finished!!")
             finishedAudio.play()
             globalTimer.invalidate()
+            
             print("Total time needed: \(totalTime)")
             self.motionManager.stopAccelerometerUpdates()
-            // ADD HERE ACTION FOR WHEN MOVEMENT IS COMPLETED!!!!
+            // Alert showinf results:
+            let alert = UIAlertController(title: "Finished!", message: "It took \(totalTime - 3) seconds to achive the needed good shaking time(\(completionScore)s)", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
         }
     }
     
     func startGettingData(){
-        //let updateInterval = 0.01 + 0.005 * self.slider.value;
+        positiveTMin = self.positiveTMinText.text!.doubleValue
+        positiveTMax = self.positiveTMaxText.text!.doubleValue
+        negativeTMin = self.negativeTMinText.text!.doubleValue
+        negativeTMax = self.negativeTMaxText.text!.doubleValue
+        fMax = self.fMaxText.text!.doubleValue
+        fMin = self.fMinText.text!.doubleValue
+        completionScore = Int(self.completionScoreText.text!.doubleValue)
+        
         self.motionManager.accelerometerUpdateInterval = TimeInterval(Constants.accInterval)
         if self.motionManager.isAccelerometerAvailable {
             globalTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.thickHandler), userInfo: nil, repeats: true)
@@ -205,23 +225,37 @@ class ViewController: UIViewController {
         secondsToBegin += 1
         if(secondsToBegin == 3)
         {
-            timer.invalidate()
+            initialTimer.invalidate()
             self.startGettingData()
         }
     }
     
     @IBAction func startPressed(_ sender: Any) {
         // Hold 3 seconds to begin analyzing data
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateTimer), userInfo: nil, repeats: true)
+        initialTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateTimer), userInfo: nil, repeats: true)
     }
     @IBAction func stopPressed(_ sender: Any) {
         if self.motionManager.isAccelerometerActive{
             self.motionManager.stopAccelerometerUpdates()
         }
+        globalTimer.invalidate()
+        initialTimer.invalidate()
+
+        globalFrequency = 0
+        globalMagnitude = (0.0, 0.0)
+        frequencyCounter = 0
+        isPositive = true
+        crossingZero = 0
+        secondsToBegin = 0
+        globalScore = 0
+        periodPoints = []
+        totalTime = 0
+        wrongMovesCounter = 0
+
+        
+        print("Stopped...")
+        
     }
-    @IBAction func sliderValueChanged(_ slider: UISlider) {
-        self.slider.value = self.slider.value
-        self.sliderLabel.text = "\(self.slider.value)"
-    }
+
 }
 
